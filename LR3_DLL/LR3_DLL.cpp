0d016c5ca02fa7c4ef0f2b16306e395f750b9e5f
Pavel Miskevich <chiki-takipavel@mail.ru>
2020-11-18 00:50:20 +0300

    Добавьте файлы проекта.

---
+++ LR3_DLL/LR3_DLL.cpp
@@ -0,0 +1,68 @@
+#include "pch.h"
+#include <Windows.h>
+#include <process.h>
+#include <vector>
+
+#define DLL_EXPORT __declspec(dllexport)
+
+using namespace std;
+
+const char SEARCH_STR[] = "TestData";
+const char REPLACEMENT[] = "Testerer";
+
+extern "C" void DLL_EXPORT __stdcall ReplaceData(DWORD pid, const char* data, const char* replacement)
+{
+	HANDLE process = OpenProcess(PROCESS_VM_READ | PROCESS_TRUST_LABEL_SECURITY_INFORMATION | PROCESS_VM_WRITE | PROCESS_QUERY_INFORMATION, FALSE, pid);
+	if (process)
+	{
+		SYSTEM_INFO si;
+		GetSystemInfo(&si);
+		MEMORY_BASIC_INFORMATION info;
+		const size_t dataLength = sizeof(data);
+		const size_t replacementLength = sizeof(replacement);
+		std::vector<char> chunk;
+		char* p = 0;
+		while (p < si.lpMaximumApplicationAddress)
+		{
+			if (VirtualQueryEx(process, p, &info, sizeof(info)) == sizeof(info) && (info.State == MEM_COMMIT && info.AllocationProtect == PAGE_READWRITE))
+			{
+				p = (char*)info.BaseAddress;
+				chunk.resize(info.RegionSize);
+				SIZE_T bytesRead;
+				if (ReadProcessMemory(process, p, &chunk[0], info.RegionSize, &bytesRead))
+				{
+					for (size_t i = 0; i < (bytesRead - dataLength); ++i)
+					{
+						if (memcmp(data, &chunk[i], dataLength) == 0)
+						{
+							char* ref = p + i;
+							for (int j = 0; j < replacementLength; j++) {
+								ref[j] = replacement[j];
+							}
+							ref[replacementLength] = 0;
+						}
+					}
+				}
+			}
+			p += info.RegionSize;
+		}
+	}
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+	DWORD pid = _getpid();
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	case DLL_THREAD_ATTACH:
+		ReplaceData(pid, SEARCH_STR, REPLACEMENT);
+		break;
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	default:
+		break;
+	}
+	return TRUE;
+}
---
+++ LR3_OSaSP/LR3_OSaSP.cpp
@@ -0,0 +1,139 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+#include <string>
+#include <vector>
+#include <TlHelp32.h>
+#include <conio.h>
+#include <fstream>
+#include <iostream>
+#include <functional>
+
+#define DLL_IMPORT __declspec(dllimport)
+#define VALUE "TestData"
+#define ARGS DWORD pid, const char* data, const char* replacement
+
+const char FUNCTION_NAME[] = "ReplaceData";
+const char SEARCH_STR[] = "TestData";
+const char REPLACEMENT[] = "Testerer";
+const char PATH_TO_DLL[] = "LR3_DLL.dll";
+
+typedef HMODULE(WINAPI* LPLoadLibrary)(LPCSTR);
+typedef HMODULE(WINAPI* LPGetProcAddress)(HMODULE, LPCSTR);
+typedef void __stdcall TReplaceData(ARGS);
+extern "C" void DLL_IMPORT __stdcall ReplaceData(ARGS);
+
+int callFunctionInStaticDll(DWORD pid);
+int callFunctionInDynamicDll(DWORD pid);
+TReplaceData* staticImport();
+TReplaceData* dynamicImport();
+void injectLibrary(DWORD procID);
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	const char data1[] = VALUE;
+	const char data2[] = VALUE;
+	DWORD pid = GetCurrentProcessId();
+
+	printf("PID: %lu\n", pid);
+	printf("Выберите тип (0 - динамически, 1 - статически, 2 - внедрение в процесс).\n");
+
+	auto a = (char)getchar();
+
+	switch (a)
+	{
+	case '0':
+		callFunctionInDynamicDll(pid);
+		break;
+	case '1':
+		callFunctionInStaticDll(pid);
+		break;
+	case '2':
+		DWORD injectionPid;
+		printf("Enter pid\n");
+		scanf_s("%d", &injectionPid);
+		injectLibrary(injectionPid);
+		break;
+	default:
+		break;
+	}
+
+	printf("Оригинал: %s. Изменённое: %s\n", VALUE, data1);
+	printf("Оригинал: %s. Изменённое: %s\n", VALUE, data2);
+
+	system("pause");
+	return 0;
+}
+
+// Вызов DLL-функции
+int makeCall(TReplaceData func, DWORD pid) {
+	if (func != NULL)
+	{
+		func(pid, SEARCH_STR, REPLACEMENT);
+		return 1;
+	}
+	else
+	{
+		puts("DLL не найдено.");
+		return 0;
+	}
+}
+
+// Статически
+int callFunctionInStaticDll(DWORD pid)
+{
+	TReplaceData* func = NULL;
+	func = staticImport();
+	return makeCall(func, pid);
+}
+
+TReplaceData* staticImport()
+{
+	return (TReplaceData*)ReplaceData;
+}
+
+// Динамически 
+int callFunctionInDynamicDll(DWORD pid)
+{
+	TReplaceData* func = NULL;
+	func = dynamicImport();
+	return makeCall(func, pid);
+}
+
+TReplaceData* dynamicImport()
+{
+	HMODULE hDll = LoadLibraryA(PATH_TO_DLL);
+	if (hDll == NULL)
+		return NULL;
+	auto* func = (TReplaceData*)GetProcAddress(hDll, FUNCTION_NAME);
+	FreeLibrary(hDll);
+	return func;
+}
+
+// Injection 
+void injectLibrary(DWORD pid)
+{
+	HMODULE hKernel = LoadLibraryA("kernel32.dll"); //статическая библиотека
+	if (hKernel) {
+		auto loadLibrary = (LPLoadLibrary)GetProcAddress(hKernel, "LoadLibraryA"); //адрес функции
+		HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
+		LPVOID path = VirtualAllocEx(hProc, NULL, strlen(PATH_TO_DLL) + 1, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+
+		if (path)
+		{
+			WriteProcessMemory(hProc, path, PATH_TO_DLL, strlen(PATH_TO_DLL) + 1, NULL);
+			HANDLE hThread = CreateRemoteThread(hProc, NULL, NULL, (LPTHREAD_START_ROUTINE)loadLibrary, path, NULL, NULL);
+
+			if (hThread == NULL) {
+				printf("Error.\n");
+			}
+			else {
+				WaitForSingleObject(hThread, INFINITE);
+				CloseHandle(hThread);
+			}
+		}
+
+		FreeLibrary(hKernel);
+		CloseHandle(hProc);
+	}
+}
